275393421e4250bff80c6dced5208f49c72642d6
buthed <temaTihonov@yandex.ru>
2021-12-09 03:34:50 +0300

    Start homework, fragment has been added

--- app/src/main/java/com/example/popular_libraries_training_project/screens/AppScreens.kt
+++ app/src/main/java/com/example/popular_libraries_training_project/screens/AppScreens.kt
@@ -1,6 +1,7 @@
 package com.example.popular_libraries_training_project.screens
 
 import com.example.popular_libraries_training_project.model.GithubUserModel
+import com.example.popular_libraries_training_project.ui.converter.ConverterFragment
 import com.example.popular_libraries_training_project.ui.users.ProfileFragment
 import com.example.popular_libraries_training_project.ui.users.UsersFragment
 import com.github.terrakok.cicerone.androidx.FragmentScreen
@@ -13,5 +14,9 @@ object AppScreens {
 
     fun userProfile(userName: GithubUserModel) = FragmentScreen {
         ProfileFragment(userName)
+    }
+
+    fun photoConverter() = FragmentScreen {
+        ConverterFragment()
     }
 }
